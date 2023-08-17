17ff8f2cf41af755c74d3b0b60d6bc78ad2e9929
Michael V. Harding <mvharding@gmail.com>
2023-08-16 19:01:51 -0700

    Initial commit, Dockerfile with Vagrantfile as well, for simple development.

---
+++ app.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    date = datetime.now()
+    now = date.strftime("%a, %m-%d-%y %H:%M:%S UTC")
+    return render_template('index.html',
+                           date = now)
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
